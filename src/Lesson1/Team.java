bc9e41aa5799af6634d7ec6969912ac20a95b1b7
Infinity2010 <arbiter2008@ya.ru>
2018-10-08 00:01:07 +0300

    класс Команда с конструктором с созданной командой и с возможностью создать свою команду из массива спортсменов

--- src/Lesson1/Team.java
+++ src/Lesson1/Team.java
@@ -1,4 +1,45 @@
 package Lesson1;
 
 public class Team {
+    private String teamName;
+    private Competitor[] competitors;
+
+    Team(String teamName) {
+        this.teamName = teamName;
+        this.competitors = new Competitor[]{
+                new Sportsman("Bob", Sex.MALE),
+                new Sportsman("John", Sex.MALE),
+                new Sportsman("Meggy", Sex.FEMALE),
+                new Sportsman("Alice", Sex.FEMALE)};
+    }
+
+    Team(String teamName, Competitor... competitors) {
+        this.teamName = teamName;
+        this.competitors = competitors;
+    }
+
+    Competitor[] getCompetitors() {
+        return competitors;
+    }
+
+    void info() {
+        for (Competitor c :
+                competitors) {
+            c.info();
+        }
+
+    }
+
+    void showResults(Obstacle... obstacles) {
+        for (Competitor c: competitors) {
+            for (Obstacle o :
+                    obstacles) {
+                o.doIt(c);
+                if(!c.isOnDistance()) {
+                    break;
+                }
+            }
+        }
+    }
+
 }
