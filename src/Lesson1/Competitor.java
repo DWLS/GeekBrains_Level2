cbf45e199925822cdab5049f44aa4fb38617ca88
Infinity2010 <arbiter2008@ya.ru>
2018-10-07 23:54:31 +0300

    интерфейс Участник

--- src/Lesson1/Competitor.java
+++ src/Lesson1/Competitor.java
@@ -1,4 +1,11 @@
 package Lesson1;
 
 public interface Competitor {
+
+    void run(int dist);
+    void swim(int dist);
+    void jump(int height);
+    boolean isOnDistance();
+    void info();
+
 }
