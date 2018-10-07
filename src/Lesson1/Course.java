bd8c12ad7ddf296ae017d3da91e37964e63c1f9c
Infinity2010 <arbiter2008@ya.ru>
2018-10-07 23:54:05 +0300

    Если участник сошёл с дистанции на одном из препятствий, то к следующему препятствию он не проходит

--- src/Lesson1/Course.java
+++ src/Lesson1/Course.java
@@ -1,4 +1,40 @@
 package Lesson1;
 
+import java.util.Random;
+
 public class Course {
+    private static Random rand = new Random();
+
+    private Obstacle[] obstacles;
+
+    Course() {
+        obstacles = new Obstacle[] {
+                new Cross(rand.nextInt(1) == 1 ? 4800 + rand.nextInt(500) : 4800 - rand.nextInt(500)),
+                new Water(rand.nextInt(1) == 1 ? 100 + rand.nextInt(20) : 100 - rand.nextInt(20)),
+                new Wall(rand.nextInt(1) == 1 ? 5 + rand.nextInt(1) : 5 - rand.nextInt(1))};
+    }
+
+    Course(Obstacle[] obstacles) {
+        this.obstacles = obstacles;
+    }
+
+    void doIt(Team team) {
+        for (Obstacle o :
+                obstacles) {
+            for (Competitor c :
+                    team.getCompetitors()) {
+                if (c.isOnDistance()) {
+                    o.doIt(c);
+                }
+            }
+        }
+    }
+
+    void info() {
+        for (Obstacle o :
+                obstacles) {
+            o.info();
+        }
+    }
+
 }
