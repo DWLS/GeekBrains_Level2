787bd923346078a2431f78de97dc5fbc9a851846
Infinity2010 <arbiter2008@ya.ru>
2018-10-08 00:03:15 +0300

    класс Спортсмен. Добавил пол спортсмена и разброс в спортивных умениях

--- src/Lesson1/MainClass.java
+++ src/Lesson1/MainClass.java
@@ -1,4 +1,28 @@
 package Lesson1;
 
 public class MainClass {
+    public static void main(String[] args) {
+        /*Competitor[] competitors = {
+                new Human("Bob"),
+                new Human("John"),
+                new Human("Meggy"),
+                new Human("Alice")};
+        Obstacle[] obstacles = {
+                new Cross(1500),
+                new Wall(5),
+                new Water(100)};*/
+
+        //Course c = new Course(obstacles);                     // Создаем полосу препятствий с параметрами
+        Course c = new Course();
+        c.info();                                               // Информация о полосе препятствий
+        //Team team = new Team("SuperTeam", competitors);       // Создаем команду с параметрами
+        Team team = new Team("SuperTeam");
+        team.info();                                            // Начальная информация по участникам
+        c.doIt(team);                                           // Просим команду пройти полосу
+        team.showResults();                                     // Показываем результаты
+        team.info();                                            // Конечная информация по участникам
+        System.out.println();
+        c.doIt(team);
+        team.info();
+    }
 }
--- src/Lesson1/Sportsman.java
+++ src/Lesson1/Sportsman.java
@@ -1,4 +1,74 @@
 package Lesson1;
 
-public class Sportsman {
+import java.util.Random;
+
+public class Sportsman implements Competitor {
+    private static Random rand = new Random();
+
+    private String name;
+    private Sex sex;
+
+    private int maxRunDistance;
+    private int maxJumpHeigth;
+    private int maxSwimDistance;
+
+    private boolean active;
+
+    Sportsman(String name, Sex sex) {
+        this.sex = sex;
+        this.name = name;
+        this.maxRunDistance = rand.nextInt(1) == 1 ? 5000 + rand.nextInt(500) : 5000 - rand.nextInt(500);
+        this.maxJumpHeigth = rand.nextInt(1) == 1 ? 5 + rand.nextInt(1) : 5 - rand.nextInt(2);
+        this.maxSwimDistance = rand.nextInt(1) == 1 ? 150 + rand.nextInt(50) : 150 - rand.nextInt(50);
+        this.active = true;
+    }
+
+    @Override
+    public void run(int dist) {
+        if (dist <= maxRunDistance) {
+            System.out.printf("%s %s с кроссом!\n", name, sex == Sex.MALE ? "справился" : "справилась");
+        } else {
+            System.out.printf("%s не %s с кроссом!\n", name, sex == Sex.MALE ? "справился" : "справилась");
+            active = false;
+        }
+    }
+
+    @Override
+    public void swim(int dist) {
+        if (dist <= maxSwimDistance) {
+            System.out.printf("%s %s с заплывом!\n", name, sex == Sex.MALE ? "справился" : "справилась");
+        } else {
+            System.out.printf("%s не %s с заплывом!\n", name, sex == Sex.MALE ? "справился" : "справилась");
+            active = false;
+        }
+    }
+
+    @Override
+    public void jump(int height) {
+        if (height <= maxJumpHeigth) {
+            System.out.printf("%s %s с высотой!\n", name, sex == Sex.MALE ? "справился" : "справилась");
+        } else {
+            System.out.printf("%s не %s с высотой!\n", name, sex == Sex.MALE ? "справился" : "справилась");
+            active = false;
+        }
+    }
+
+    @Override
+    public boolean isOnDistance() {
+        return active;
+    }
+
+    @Override
+    public void info() {
+        if(!isOnDistance()) {
+            System.out.printf("%s %s с дистанции\n", name, sex == Sex.MALE ? "сошёл" : "сошла");
+        } else {
+            System.out.printf("%s может пробежать %d метров, может проплыть %d метров, может подпрыгнуть на %d метров.\n",
+                    name,
+                    maxRunDistance,
+                    maxSwimDistance,
+                    maxJumpHeigth);
+        }
+    }
+
 }
