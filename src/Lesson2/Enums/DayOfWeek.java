ca6262039365812c968fedbffb0511a8a1eb0208
Eternity86 <F0HTp[W{p:3!Y6>
2018-10-10 02:08:36 +0300

    добавил конструктор с русским названием дня недели, а также геттер для получения русского названия дня недели

--- src/Lesson2/Enums/DayOfWeek.java
+++ src/Lesson2/Enums/DayOfWeek.java
@@ -1,11 +1,21 @@
 package Lesson2.Enums;
 
 public enum DayOfWeek {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MONDAY("Понедельник"),
+    TUESDAY("Вторник"),
+    WEDNESDAY("Среда"),
+    THURSDAY("Четверг"),
+    FRIDAY("Пятница"),
+    SATURDAY("Суббота"),
+    SUNDAY("Воскресенье");
+
+    private String rusDayOfWeek;
+
+    DayOfWeek(String s) {
+        rusDayOfWeek = s;
+    }
+
+    String getRusDayOfWeek() {
+        return rusDayOfWeek;
+    }
 }
