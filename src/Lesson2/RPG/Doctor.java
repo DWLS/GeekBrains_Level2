6c834e29684bea49992242bc9913f2f6d99b8198
Eternity86 <F0HTp[W{p:3!Y6>
2018-10-10 02:27:31 +0300

    класс Doctor вынесен в отдельный файл

--- src/Lesson2/RPG/Doctor.java
+++ src/Lesson2/RPG/Doctor.java
@@ -1,4 +1,18 @@
 package Lesson2.RPG;
 
-public class Doctor {
+class Doctor extends Hero {
+
+    Doctor(int heal, String name, int damage, int addHeal) {
+        super(heal, name, damage, addHeal);
+    }
+
+    @Override
+    void hit(Hero hero) {
+        System.out.println("Доктор не может бить!");
+    }
+
+    @Override
+    void healing(Hero hero) {
+        hero.addHealth(addHeal);
+    }
 }
