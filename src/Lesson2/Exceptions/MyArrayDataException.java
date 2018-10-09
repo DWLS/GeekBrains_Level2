1afa905abb86394d6aa62a0f77299aa7c47f6c29
Infinity2010 <arbiter2008@ya.ru>
2018-10-09 23:49:00 +0300

    модифицировал вывод ообщения об ошибке

--- src/Lesson2/Exceptions/MyArrayDataException.java
+++ src/Lesson2/Exceptions/MyArrayDataException.java
@@ -2,11 +2,7 @@ package Lesson2.Exceptions;
 
 class MyArrayDataException extends RuntimeException {
     MyArrayDataException(String msg) {
-        super(msg);
+        super("Ошибка преобразования элемента массива в целое число.\n" + " " + msg);
     }
 
-    @Override
-    public String toString() {
-        return "Невозможно найти сумму элементов массива.";
-    }
 }
