015ca36c47e562cf8d338bfce26dfba3c8c2fbf4
Infinity2010 <arbiter2008@ya.ru>
2018-10-10 00:12:02 +0300

    заготовка для решения задачи с перечислениями

--- src/Lesson2/Enums/DayOfWeekMain.java
+++ src/Lesson2/Enums/DayOfWeekMain.java
@@ -1,4 +1,22 @@
 package Lesson2.Enums;
 
+/*
+Требуется реализовать enum DayOfWeek, который будет представлять дни недели.
+С его помощью необходимо решить задачу определения кол-ва рабочих часов до конца недели по заднному текущему дню.
+
+ Возвращает кол-во оставшихся рабочих часов до конца
+ недели по заданному текущему дню. Считается, что
+ текущий день ещё не начался, и рабочие часы за него
+ должны учитываться.
+ */
+
 public class DayOfWeekMain {
+
+    public static void main(String[] args) {
+        System.out.println(getWorkingHours(DayOfWeek.MONDAY));
+    }
+
+    private static int getWorkingHours(DayOfWeek dof) {
+        return 0;
+    }
 }
