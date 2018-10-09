a27846acda028684f91716a75aae23876d28fb88
Eternity86 <F0HTp[W{p:3!Y6>
2018-10-10 02:27:55 +0300

    класс Assassin вынесен в отдельный файл

--- src/Lesson2/RPG/Assassin.java
+++ src/Lesson2/RPG/Assassin.java
@@ -1,4 +1,31 @@
 package Lesson2.RPG;
 
-public class Assassin {
+import java.util.Random;
+
+class Assassin extends Hero {
+
+    private int cricitalHit;
+
+    Assassin(int heal, String name, int damage, int addHeal) {
+        super(heal, name, damage, addHeal);
+        Random random = new Random();
+        this.cricitalHit = random.nextInt(20);
+    }
+
+    @Override
+    void hit(Hero hero) {
+        if (hero != this) {
+            if(health < 0) {
+                System.out.println("Герой погиб и бить не может!");
+            } else {
+                hero.causeDamage(damage + cricitalHit);
+            }
+            System.out.println(this.name + " нанес урон " + hero.name);
+        }
+    }
+
+    @Override
+    void healing(Hero hero) {
+        System.out.println("Убийцы не умеют лечить!");
+    }
 }
