f82480a90c6bd3c17803494ac2bc17c05eaeffd7
Eternity86 <F0HTp[W{p:3!Y6>
2018-10-10 02:28:22 +0300

    абстрактный файл Hero вынесен в отдельный файл

--- src/Lesson2/RPG/Hero.java
+++ src/Lesson2/RPG/Hero.java
@@ -1,4 +1,41 @@
 package Lesson2.RPG;
 
-public class Hero {
+abstract class Hero {
+
+    int health;
+    String name;
+    int damage;
+    int addHeal;
+
+    Hero(int health, String name, int damage, int addHeal) {
+        this.health = health;
+        this.name = name;
+        this.damage = damage;
+        this.addHeal = addHeal;
+    }
+
+    abstract void hit(Hero hero);
+
+    abstract void healing(Hero hero);
+
+    void causeDamage(int damage) {
+        if(health < 0) {
+            System.out.println("Герой уже мертвый!");
+        } else {
+            health -= damage;
+        }
+
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    void addHealth(int health) {
+        this.health += health;
+    }
+
+    void info() {
+        System.out.println(name + " " + (health < 0 ? "Герой мертвый" : health) + " " + damage);
+    }
 }
