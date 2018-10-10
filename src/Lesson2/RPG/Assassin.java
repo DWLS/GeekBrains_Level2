588bc4de80d22d03d28dfae8b43931097af4bb6a
Eternity86 <arbiter2008@ya.ru>
2018-10-11 02:23:32 +0300

    изменения с учётом параметра maxHealth

--- src/Lesson2/RPG/Assassin.java
+++ src/Lesson2/RPG/Assassin.java
@@ -4,11 +4,10 @@ import java.util.Random;
 
 class Assassin extends Hero {
 
-    private int criticalHit;
     private Random critHit = new Random();
 
-    Assassin(int health, String name, int damage, int addHeal) {
-        super(health, name, damage, addHeal);
+    Assassin(int maxHealth, String name, int damage, int addHeal) {
+        super(maxHealth, name, damage, addHeal);
     }
 
     @Override
@@ -17,7 +16,7 @@ class Assassin extends Hero {
             if(this.health <= 0) {
                 System.out.printf("%s погиб и бить не может!\n", this.name);
             } else {
-                int currentDamage = damage + critHit.nextInt(20);
+                int currentDamage = damage + critHit.nextInt(20);   // уровень убийцы на каждом ходу равен "базовый уровень + случайный критический урон"
                 hero.causeDamage(currentDamage);
                 System.out.printf("%s(%d) наносит %d урона %s(%d)\n", this.name, this.getHealth(), currentDamage, hero.name, hero.getHealth());
 
