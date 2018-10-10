26db92398f5bc78a529d6a945efd9742b47e2e3e
Eternity86 <arbiter2008@ya.ru>
2018-10-10 23:19:41 +0300

    вывод информации о том, герой какой команды совершает ход

--- src/Lesson2/RPG/Game.java
+++ src/Lesson2/RPG/Game.java
@@ -8,27 +8,33 @@ class Game {
         Random randomStep = new Random();
         Random randomHealing = new Random();
         Random randomHit = new Random();
-        int round = 8;
+        int round = 25;
 
-        Hero[] team1 = new Hero[]{new Warrior(250, "Тигрил", 50, 0),
+        Hero[] team1 = new Hero[] {
+                new Warrior(250, "Тигрил", 50, 0),
                 new Assassin(150, "Акали", 70, 0),
-                new Doctor(120, "Жанна", 0, 70)};
+                new Doctor(120, "Жанна", 0, 70)
+        };
 
 
-        Hero[] team2 = new Hero[]{new Warrior(240, "Минотавр", 60, 0),
+        Hero[] team2 = new Hero[] {
+                new Warrior(240, "Минотавр", 60, 0),
                 new Assassin(160, "Джинкс", 60, 0),
-                new Doctor(110, "Зои", 0, 80)};
+                new Doctor(110, "Зои", 0, 80)
+        };
 
 
         for (int j = 0; j < round; j++) {
             for (int i = 0; i < team1.length; i++) {
                 if(randomStep.nextInt(2) == 0) {
+                    System.out.print("Ход игрока первой команды: ");
                     if(team1[i] instanceof Doctor) {
                         team1[i].healing(team1[randomHealing.nextInt(2)]);
                     } else {
                         team1[i].hit(team2[randomHit.nextInt(3)]);
                     }
                 } else {
+                    System.out.print("Ход игрока второй команды: ");
                     if(team2[i] instanceof Doctor) {
                         team2[i].healing(team2[randomHealing.nextInt(2)]);
                     } else {
