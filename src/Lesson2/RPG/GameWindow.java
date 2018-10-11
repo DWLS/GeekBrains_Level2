a91c21cdf2b3f39fa4ce03ff90643365108c4fa2
Eternity86 <arbiter2008@ya.ru>
2018-10-11 16:17:30 +0300

    класс GameWindow с перечислением основных необходимых элементов GUI

--- src/Lesson2/RPG/GameWindow.java
+++ src/Lesson2/RPG/GameWindow.java
@@ -1,4 +1,20 @@
 package Lesson2.RPG;
 
-public class GameWindow {
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    JButton btn1 = new JButton("Добавить");
+    JButton btn2 = new JButton("Добавить");
+    JButton btn3 = new JButton("НАЧАТЬ");
+    JComboBox cb1 = new JComboBox();
+    JComboBox cb2 = new JComboBox();
+    JTextField tf1 = new JTextField();
+    JLabel lbl1 = new JLabel("Команда 1");
+    JLabel lbl2 = new JLabel("Команда 2");
+    JLabel lbl3 = new JLabel("Количество раундов");
+    JLabel lbl4 = new JLabel("Результаты сражения:");
+    JList lst1 = new JList();
+    JList lst2 = new JList();
+    JList lst3 = new JList();
+    JMenuBar mb1 = new JMenuBar();
 }
