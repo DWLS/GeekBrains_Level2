0e400c258496d82878a8c6577e0e3777fafbd873
Eternity86 <arbiter2008@ya.ru>
2018-10-14 19:41:11 +0300

    вынес класс ТелефоннаяКнига в отдельный файл реализованы методы: - addRecord() - добавить запись - delRecord() - удалить запись - print() - распечатать телевонную книгу - getSurname() - получить фамилию абонента по номеру - getNumber() - получить номер телефона по фамилии абонента, если в справочнике есть однофамильцы, то выводит все пары Фамилия: Номер

--- src/Lesson3/PhoneBook/PhoneBook.java
+++ src/Lesson3/PhoneBook/PhoneBook.java
@@ -1,4 +1,4 @@
-package Lesson3;
+package Lesson3.PhoneBook;
 
 /*
 2. Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
@@ -10,37 +10,60 @@ package Lesson3;
 
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.TreeMap;
 
-public class PhoneBook {
-    private static final String COUNTRY_CODE_RU = "+7";
-    public static void main(String[] args) {
-        // ключом будет номер телефона, т.к. он не может повторяться, а значением будут фамилии, т.к. возможны однофамильцы
-        Map<String, String> phoneBook = new TreeMap<>();
-        add(phoneBook, "9005568589", "Ivanov");
-        add(phoneBook, "9005568592", "Petrov");
-        add(phoneBook, "9005568593", "Sidorov");
-        add(phoneBook, "9005568594", "Solov'yov");
-        add(phoneBook, "9005568595", "Petrov");
-        add(phoneBook, "9005568599", "Zverev");
-        add(phoneBook, "9005568685", "Sergeev");
-        add(phoneBook, "9005568966", "Ivanchenko");
-        add(phoneBook, "9005568485", "Shutov");
-        add(phoneBook, "9005568789", "Ivanov");
-        for (String s :
-                phoneBook.keySet()) {
-            System.out.println(phoneBook.get(s) + " " + s);
+class PhoneBook {
+    // импровизированная БД на основании HashMap:
+    // в качестве ключа используем номер телефона, т.к. он уникальный,
+    // а значением будут фамилии т.к. возможны однофамильцы.
+    // если использовать TreeMap, телефонная книга будет упорядочена по ключу, т.е. номеру телефона
+    private Map<String, String> phoneBook;
+    // код страны - просто для большей реалистичности
+    private final String COUNTRY_CODE_RU = "+7";
+
+    PhoneBook(Map<String, String> map) {
+        phoneBook = map;
+    }
+
+    // добавляет запись с номером телефона и фамилией в импровизированную БД
+    void addRecord(String phone, String surname) {
+        phoneBook.put(COUNTRY_CODE_RU + phone, surname);
+    }
+
+    // удаляет запись по номеру телефона
+    void delRecord(String phone) {
+        phoneBook.remove(COUNTRY_CODE_RU + phone);
+    }
+
+    // выводит на печать телефонный справочник
+    void print() {
+        System.out.println("Телефонный справочник:");
+        System.out.println("======================");
+        phoneBook.forEach((key, value) -> System.out.println(value + ": " + key));
+    }
+
+    // поиск фамилии абонента по номеру телефона
+    // метода нет в задании, но для полноты, кмк, он не помешает
+    String getSurname(String number) {
+        String result = phoneBook.get(COUNTRY_CODE_RU + number);
+        if (result == null) {
+            return "Абонент с таким номером не найден.";
         }
-
-        get(phoneBook, "Ivanov");
-
+        return result;
     }
 
-    private static void add(Map<String, String> hm, String phoneNumber, String surname) {
-        hm.put(COUNTRY_CODE_RU + phoneNumber, surname);
+    // поиск номера телефона (или же номеров телефонов) по фамилии
+    // в случае однофамильцев возвращает все телефоны под этой фамилией
+    ArrayList<String> getNumber(String surname) {
+        ArrayList<String> result = new ArrayList<>();
+        phoneBook.forEach((key, value) -> {
+            if (surname.equalsIgnoreCase(value)) {
+                result.add(value + ": " + key);
+            }
+        });
+        if (result.size() == 0) {
+            result.add("Абонент с такой фамилией не найден");
+        }
+        return result;
     }
 
-    static ArrayList<String> get(Map<String, String> map, String surname) {
-        return new ArrayList<>();
-    }
 }
