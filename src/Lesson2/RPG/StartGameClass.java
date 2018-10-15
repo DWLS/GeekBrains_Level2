e980e0f01e66b20e7df905bd715255d4dfc15c58
Eternity86 <arbiter2008@ya.ru>
2018-10-15 12:21:37 +0300

    главный класс, запускающий игру

--- src/Lesson2/RPG/StartGameClass.java
+++ src/Lesson2/RPG/StartGameClass.java
@@ -1,4 +1,18 @@
 package Lesson2.RPG;
 
+import javax.swing.*;
+
 public class StartGameClass {
+    public static void main(String[] args) {
+        // For Native Look and Feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(Exception ignored){}
+
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                new GameWindow();
+            }
+        });
+    }
 }
