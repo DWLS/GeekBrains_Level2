89aa8a04f3380187dd280ec9d77054f55f1c320b
Eternity86 <arbiter2008@ya.ru>
2018-10-15 12:15:20 +0300

    в методе hit() показывается изначальное (до удара) и конечное (после удара) кол-во здоровья у игрока противника

--- src/Lesson2/RPG/Warrior.java
+++ src/Lesson2/RPG/Warrior.java
@@ -7,21 +7,24 @@ class Warrior extends Hero {
     }
 
     @Override
-    void hit(Hero hero) {
+    String hit(Hero hero) {
         if (hero != this) {
-            if(this.health <= 0) {
+            if (this.health <= 0) {
                 System.out.printf("%s погиб и бить не может!\n", this.name);
+                return String.format("%s погиб и бить не может!\n", this.name);
             } else {
-                hero.causeDamage(damage);
-                System.out.printf("%s(%d) наносит %d урона %s(%d)\n", this.name, this.getHealth(), damage, hero.name, hero.getHealth());
-
-            }
-
+            int heroHealth = hero.getHealth();
+            hero.causeDamage(damage);
+            System.out.printf("%s(%d) наносит %d урона %s(%d -> %d)\n", this.name, this.getHealth(), damage, hero.name, heroHealth, hero.getHealth());
+            return String.format("%s(%d) наносит %d урона %s(%d -> %d)\n", this.name, this.getHealth(), damage, hero.name, heroHealth, hero.getHealth());
         }
+    }
+        return "";
     }
 
     @Override
-    void healing(Hero hero) {
+    String healing(Hero hero) {
         System.out.println("Воины не умеют лечить!");
+        return "Воины не умеют лечить!";
     }
 }
