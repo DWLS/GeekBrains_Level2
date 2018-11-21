f843f1cba28f3d169d63183472468dc6122fd8b6
Infinity86 <arbiter2008@ya.ru>
2018-11-21 15:51:45 +0300

    Внесение правок в метод авторизации БД и классе ClientHandler

--- src/Lesson6/aChat/server/AuthService.java
+++ src/Lesson6/aChat/server/AuthService.java
@@ -1,43 +1,44 @@
 package Lesson6.aChat.server;
 
 import java.sql.*;
-import java.util.Objects;
 
 class AuthService {
     private static Connection connection;
     private static Statement stmt;
 
-    static void connect() throws SQLException {
+    static void connect() {
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:mainDB25102018.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:users.db");
             System.out.println("БД подключена!");
             stmt = connection.createStatement();
             System.out.println("Соединение с БД установлено!");
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static void addUser(String login, String pass, String nick) throws SQLException {
-        String sql = String.format("INSERT INTO USERS (login, password, nickname)" +
-                "VALUES ('%s','%s','%s')", login, pass.hashCode(), nick);
-        stmt.execute(sql);
+    public static void addUser(String login, String pass, String nick) {
+        try {
+            String query = "INSERT INTO main (login, password, nickname) VALUES (?, ?, ?);";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, login);
+            ps.setInt(2, pass.hashCode());
+            ps.setString(3, nick);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static String getNickByLoginAndPass(String login, String pass) {
-        String sql = String.format("SELECT nickname, password FROM USERS" +
-                " WHERE login = '%s'", login);
         try {
+            ResultSet rs = stmt.executeQuery("SELECT nickname, password FROM main WHERE login = '" + login + "'");
             int myHash = pass.hashCode();
-
-            ResultSet rs = stmt.executeQuery(sql);
-
-            if(rs.next()) {
+            if (rs.next()) {
                 String nick = rs.getString(1);
                 int dbHash = rs.getInt(2);
-
-                if(myHash == dbHash) {
+                if (myHash == dbHash) {
                     return nick;
                 }
             }
@@ -49,8 +50,10 @@ class AuthService {
 
     static void disconnect() {
         try {
-            connection.close();
-            System.out.println("Соединение с БД закрыто!");
+            if (connection != null) {
+                connection.close();
+                System.out.println("Соединение с БД закрыто!");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
